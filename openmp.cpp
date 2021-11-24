b562ef0fe1091aad864bcfbbf139ba914cf1dcf2
Colin Rioux <colin350@gmail.com>
2021-11-23 22:41:09 -0500

    omp scaffold

---
+++ openmp.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file openmp.cpp
+ * @author Colin Rioux <crioux@scu.edu>
+ * @author David Krakauer
+ * @author Alexandra Kissas
+ * 
+ */
+
+# include <omp.h>
+# include <iostream>
+# include <stdio.h>
+# include <stdlib.h>
+# include <assert.h>
+# include <time.h>
+# include <cstring>
+
+int main(int argc, char **argv)
+{
+    
+    return EXIT_SUCCESS;
+}
