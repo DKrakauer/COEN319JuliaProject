2e59302d5d45e5b01eee41207f7f87fb5434d964
Colin Rioux <colin350@gmail.com>
2021-12-02 19:36:18 -0800

    slurm script

--- scripts/s_fw_julia.sh
+++ scripts/s_fw_julia.sh
@@ -1,10 +1,15 @@
-#!/bin/bash -l
+#!/bin/bash
+#
+#SBATCH --job-name=dkproj
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=56
+#SBATCH --mem-per-cpu=8G
 #SBATCH --nodes=1
-#SBATCH --ntasks=28
-#SBATCH --ntasks-per-node=2
-#SBATCH --time=00:10:00
-#SBATCH --output=myoutput.log
-#SBATCH --job-name=my-julia-job
+#SBATCH --output=logs/log-%j.out
+#SBATCH --time=3:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dkrakauer@scu.edu
+#
 
 source /WAVE/users/unix/dkrakauer/julia
 cd working/folder/of/your/choice
